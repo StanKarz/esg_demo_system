705f2f1cf27d0fd533677be792718415ad995917
StanKarz <stan.karzhev@gmail.com>
2023-05-23 20:12:49 +0100

    Working report visualisation as tree structure along with code to convert JSON to tree structure

---
+++ data_processing.py
@@ -0,0 +1,69 @@
+from anytree import Node, RenderTree
+import json
+
+
+# Function to read JSONL file
+import json
+
+def read_jsonl(input_path: str) -> list:
+    """
+    Read a .jsonl file and return the data.
+
+    :param input_path: The path to the .jsonl file.
+    :return: The data from the .jsonl file.
+    """
+    data = []
+    with open(input_path, 'r', encoding='utf-8') as json_file:
+        for line in json_file:
+            data.append(json.loads(line))
+    return data
+
+
+
+
+def build_tree(data: list) -> dict:
+    """
+    Build a tree structure from the list of dictionaries.
+
+    :param data: A list of lists where each sublist contains a dictionary with node information.
+    :return: A dictionary mapping node id to node object.
+    """
+    id_to_node = {}
+
+    # First, create nodes
+    for sublist in data:
+        for node_dict in sublist:
+            node_id = node_dict['self_id']
+            node_text = node_dict.get('text_str', "")[:100]
+            id_to_node[node_id] = Node(node_id, parent=None, text_str=node_text)
+
+    # Second, link nodes to their parents
+    for sublist in data:
+        for node_dict in sublist:
+            node = id_to_node[node_dict['self_id']]
+            if node_dict['parent_id']:
+                node.parent = id_to_node[node_dict['parent_id']]
+
+    return id_to_node
+
+
+
+
+# Function to convert tree to D3.js format
+def convert_to_d3_format(node):
+    return {
+        "name": node.text_str,
+        "children": [convert_to_d3_format(child) for child in node.children]
+    }
+
+#Execute functions
+data = read_jsonl('tree_text_sample_16032023/0c2cf15bf82ae16d05ba78dc3c64b36cb87cc263.jsonl')
+id_to_node = build_tree(data)
+root = [node for node in id_to_node.values() if node.is_root][0] # Get the root node
+d3_tree = convert_to_d3_format(root)
+
+# Save to JSON file
+with open('tree.json', 'w') as f:
+    json.dump(d3_tree, f)
+
+
