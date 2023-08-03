b04a7534ca007269cdf5ef7e1f9cc5209cb794e2
StanKarz <stan.karzhev@gmail.com>
2023-08-03 21:53:42 +0100

    removed navigation from word clouds and bubble chart

--- front_end/src/pages/WordFrequency.js
+++ front_end/src/pages/WordFrequency.js
@@ -1,66 +1,65 @@
-import React, { useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import WordCloud from './WordCloud';
-import BubbleChart from './BubbleChart';
+import React, { useState } from "react";
+import WordCloud from "./WordCloud";
+import BubbleChart from "./BubbleChart";
 
 function WordFrequency() {
-    const { filename } = useParams();
-    const [selectedFile, setSelectedFile] = useState();
-    const [selectedCategory, setSelectedCategory] = useState('environmental');
-    const navigate = useNavigate();
+  const [selectedFile, setSelectedFile] = useState();
+  const [filepath, setFilepath] = useState();
+  const [selectedCategory, setSelectedCategory] = useState("environmental");
 
-    const handleFileChange = (event) => {
-        setSelectedFile(event.target.files[0]);
-    };
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
 
-    const handleFileUpload = () => {
-        if (!selectedFile) {
-            alert('No file selected!');
-            return;
-        }
+  const handleFileUpload = () => {
+    if (!selectedFile) {
+      alert("No file selected!");
+      return;
+    }
 
-        const formData = new FormData();
-        formData.append('pdf', selectedFile);
+    const formData = new FormData();
+    formData.append("pdf", selectedFile);
 
-        fetch('http://localhost:3000/upload-wordcloud', {
-            method: 'POST',
-            body: formData
-        })
-        .then(response => response.json())
-        .then(data => {
-            navigate(`/visualisations/word_frequency/${selectedFile.name}`);
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-    };
+    fetch("http://localhost:3000/upload-wordcloud", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setFilepath(selectedFile.name);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
 
-    const filepath = filename || (selectedFile ? selectedFile.name : '');
-
-    return (
-        <div>
-            <h1>Word Frequency Visualisation</h1>
-            {!filename && (
-                <>
-                    <input type="file" accept=".pdf" onChange={handleFileChange} />
-                    <button onClick={handleFileUpload}>Upload</button>
-                </>
-            )}
-            {filename && (
-                <>
-                    <select value={selectedCategory} onChange={e => setSelectedCategory(e.target.value)}>
-                        <option value="environmental">Environmental Words</option>
-                        <option value="social">Social Words</option>
-                        <option value="governance">Governance Words</option>
-                    </select>
-                    <div style={{display: 'block'}}>
-                        <WordCloud filepath={filepath} category={selectedCategory} />
-                        <BubbleChart filepath={filepath} />
-                    </div>
-                </>
-            )}
-        </div>
-    );
+  return (
+    <div>
+      <h1>Word Frequency Visualisation</h1>
+      {!filepath && (
+        <>
+          <input type="file" accept=".pdf" onChange={handleFileChange} />
+          <button onClick={handleFileUpload}>Upload</button>
+        </>
+      )}
+      {filepath && (
+        <>
+          <select
+            value={selectedCategory}
+            onChange={(e) => setSelectedCategory(e.target.value)}
+          >
+            <option value="environmental">Environmental Words</option>
+            <option value="social">Social Words</option>
+            <option value="governance">Governance Words</option>
+          </select>
+          <div style={{ display: "block" }}>
+            <WordCloud filepath={filepath} category={selectedCategory} />
+            <BubbleChart filepath={filepath} />
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default WordFrequency;
